e8d397ddfceb10204e83e8014776b4f2f0e975d6
codecv <os7blue@163.com>
2022-10-20 01:01:07 +0800

    change ui library to layui-vue

--- view/static/js/config.js
+++ view/static/js/config.js
@@ -1,37 +1,39 @@
 //插件形式封装一些工具
-const yd = {};
+
+axios.defaults.withCredentials=true
 const axiosInstance = axios.create({
     timeout: 600000,
     headers: {
         'Content-Type': 'application/json'
     }
 });
-yd.install = function (Vue, options) {
 
-    Vue.prototype.$request = {
-        get: (url = '', data = {}) => {
-            return new Promise((resolve, reject) => {
-                axiosInstance.get(url, JSON.stringify(data)).then(res => {
-                    resolve(res.data)
-                }).catch(err => {
-                    reject(err.data)
+const http = {
+    install(app, options) {
+        app.config.globalProperties.$request = {
+            get: (url = '', data = {}) => {
+                return new Promise((resolve, reject) => {
+                    axiosInstance.get(url, JSON.stringify(data)).then(res => {
+                        resolve(res.data)
+                    }).catch(err => {
+                        reject(err.data)
+                    })
                 })
-            })
-        },
-        post: (url = '', data = {}) => {
-            console.log(data)
-            return new Promise((resolve, reject) => {
-                axiosInstance.post(url, JSON.stringify(data)).then(res => {
-                    resolve(res.data)
-                }).catch(err => {
-                    reject(err.data)
+            },
+            post: (url = '', data = {}) => {
+
+                return new Promise((resolve, reject) => {
+                    axiosInstance.post(url, JSON.stringify(data)).then(res => {
+                        console.log(res)
+                        resolve(res.data)
+                    }).catch(err => {
+                        reject(err.data)
+                    })
                 })
-            })
+            }
+
         }
 
+
     }
-
-
 }
-
-Vue.use(yd);
