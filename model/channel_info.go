522cfb35a08387c07340eba774dbca5dedd49558
os7blue <os7blue@163.com>
2022-11-12 22:24:15 +0800

    channel plan init

--- model/channel_info.go
+++ model/channel_info.go
@@ -3,12 +3,9 @@ package model
 //item -> line -> info
 
 type ChannelInfo struct {
-	ID   uint   `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name string `gorm:"not null;size:20;unique" json:"name"`
-	//status code , enable is 10 , disabled is 20
-	Key           string `gorm:"not null;unique" json:"key"`
+	ID            uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name          string `gorm:"not null;size:20;unique" json:"name"`
 	CreateTime    int64  `gorm:"not null;autoCreateTime:milli" json:"createTime"`
-	WhiteListStr  string `json:"whiteListStr"`
 	ChannelType   int    `gorm:"not null" json:"channelType"`
 	OptionJsonStr string `gorm:"not null" json:"optionJsonStr"`
 }
@@ -16,9 +13,7 @@ type ChannelInfo struct {
 type ChannelInfoView[T any] struct {
 	ID   uint   ` json:"id"`
 	Name string ` json:"name"`
-	//status code , enable is 10 , disabled is 20
-	Status        int    `json:"status"`
-	Key           string `json:"key"`
+
 	CreateTime    int64  `json:"createTime"`
 	WhiteListStr  string `json:"whiteListStr"`
 	ChannelType   int    `json:"channelType"`
@@ -36,7 +31,6 @@ type ChannelInfoUpdateValidator struct {
 	ID            uint   `json:"id" binding:"required"`
 	Name          string `json:"name" binding:"omitempty,min=2,max=20"`
 	WhiteListStr  string `json:"whiteListStr"`
-	Status        int    `json:"status"`
 	OptionJsonStr string `json:"optionJsonStr"`
 }
 
