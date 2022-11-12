ee6c0bbf93c99e81bb15d46d8a972dc55172593b
os7blue <os7blue@163.com>
2022-11-12 22:11:43 +0800

    dev innit

--- model/channel_info.go
+++ model/channel_info.go
@@ -6,7 +6,6 @@ type ChannelInfo struct {
 	ID   uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name string `gorm:"not null;size:20;unique" json:"name"`
 	//status code , enable is 10 , disabled is 20
-	Status        int    `gorm:"not null;size:2" json:"status"`
 	Key           string `gorm:"not null;unique" json:"key"`
 	CreateTime    int64  `gorm:"not null;autoCreateTime:milli" json:"createTime"`
 	WhiteListStr  string `json:"whiteListStr"`
