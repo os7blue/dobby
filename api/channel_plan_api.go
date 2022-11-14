8a953807e525460ba18f57c7205c6bff7afda88d
os7blue <os7blue@163.com>
2022-11-14 12:48:13 +0800

    channel plan initd

---
+++ api/channel_plan_api.go
@@ -0,0 +1,30 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+type channelPlanApi struct {
+}
+
+func (p *channelPlanApi) Create(*gin.Context) {
+
+}
+
+func (p *channelPlanApi) Load(context *gin.Context) {
+
+}
+
+func (p *channelPlanApi) Update(context *gin.Context) {
+
+}
+
+func (p *channelPlanApi) ChangeStatus(context *gin.Context) {
+
+}
+
+func (p *channelPlanApi) Delete(context *gin.Context) {
+
+}
+
+func (p *channelPlanApi) RefreshKey(context *gin.Context) {
+
+}
--- model/channel_plan.go
+++ model/channel_plan.go
@@ -4,7 +4,21 @@ type ChannelPlan struct {
 	ID   uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name string `gorm:"not null;size:20;unique" json:"name"`
 	//status code , enable is 10 , disabled is 20
-	Key          string `gorm:"not null;unique" json:"key"`
-	CreateTime   int64  `gorm:"not null;autoCreateTime:milli" json:"createTime"`
-	WhiteListStr string `json:"whiteListStr"`
+	Key            string `gorm:"not null;unique" json:"key"`
+	CreateTime     int64  `gorm:"not null;autoCreateTime:milli" json:"createTime"`
+	WhiteListStr   string `json:"whiteListStr"`
+	ChannelListStr string `json:"channelListStr"`
+}
+
+type ChannelPlanCreateValidator struct {
+	Name           string `json:"name"`
+	WhiteListStr   string `json:"whiteListStr" binding:"required"`
+	ChannelListStr string `json:"channelListStr" binding:"required"`
+}
+
+type ChannelPlanUpdateValidator struct {
+	ID             uint   `json:"id" bind:"required"`
+	Name           string `json:"name"`
+	WhiteListStr   string `json:"whiteListStr" binding:"required"`
+	ChannelListStr string `json:"channelListStr" binding:"required"`
 }
